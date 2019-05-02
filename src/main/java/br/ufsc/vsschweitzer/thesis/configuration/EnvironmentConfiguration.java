159295f5cf57eb2a79dae554bfc59fbc05e315ec
vinicius.ss <schweitzer.vinicius@gmail.com>
2019-05-02 17:47:26 -0300

    Fix regular expression for ip addresses in the configuration class

--- src/main/java/br/ufsc/vsschweitzer/thesis/configuration/EnvironmentConfiguration.java
+++ src/main/java/br/ufsc/vsschweitzer/thesis/configuration/EnvironmentConfiguration.java
@@ -7,7 +7,7 @@ public class EnvironmentConfiguration {
 
 	private static final String DEFAULT_IP_ADDRESS = "localhost";
 	private static final String DEFAULT_PORT = "12345";
-	private static final String IP_ADDRESS_REGEX = "\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}";
+	private static final String IP_ADDRESS_REGEX = "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}";
 	private static final String PORT_REGEX = "\\d{1,5}";
 
 	private String ipAddress;
